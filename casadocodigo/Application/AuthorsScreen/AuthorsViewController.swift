e8366c8d80016082322d35c184526614a5b98f1a
Rafael Rollo <rafaelrollo92@gmail.com>
2021-04-09 22:28:01 -0300

    Added the authors view controller

---
+++ casadocodigo/Application/AuthorsScreen/AuthorsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AuthorsViewController.swift
+//  casadocodigo
+//
+//  Created by rafael.rollo on 09/04/21.
+//
+
+import UIKit
+
+class AuthorsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        StatusBarBackground(target: self.view).set(color: NavigationBar.COLOR)
+    }
+
+}
