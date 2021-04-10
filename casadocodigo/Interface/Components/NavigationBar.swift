6548c4917264ff8903cb170f39098502b68765a9
Rafael Rollo <rafaelrollo92@gmail.com>
2021-04-09 22:22:21 -0300

    Added the custom navigation bar component

---
+++ casadocodigo/Interface/Components/NavigationBar.swift
@@ -0,0 +1,45 @@
+//
+//  NavigationBar.swift
+//  casadocodigo
+//
+//  Created by rafael.rollo on 09/04/21.
+//
+
+import UIKit
+
+class NavigationBar: UIView {
+    
+    static let COLOR = UIColor(
+        red: 255/255,
+        green: 170/255,
+        blue: 86/255,
+        alpha: 0.8
+    )
+    
+    // MARK: IBOutlets
+    
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var logoImageView: UIImageView!
+    
+    // for using the custom view in code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    // for using the custom view in interface builder
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    private func setup() {
+        Bundle.main.loadNibNamed("NavigationBar", owner: self, options: nil)
+        
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        addSubview(contentView)
+        
+        logoImageView.image = UIImage(named: "logo-cdc")
+    }
+}
