48dde6f01640e3d2d72bb32d893e5fdbd54aa9ab
Rafael Rollo <rafaelrollo92@gmail.com>
2021-04-15 20:52:35 -0300

    Created the repository to get book listing

---
+++ casadocodigo/Domain/Books/BookRepository.swift
@@ -0,0 +1,33 @@
+//
+//  BookRepository.swift
+//  casadocodigo
+//
+//  Created by rafael.rollo on 14/04/21.
+//
+
+import UIKit
+import Alamofire
+
+class BookRepository: NSObject {
+
+    static let bookBasePath = "https://casadocodigo-api.herokuapp.com/api/book"
+    
+    func showcase(completionHandler: @escaping ([BookShowcaseItem]) -> Void) {
+        let headers: HTTPHeaders = [
+            "Accept": "application/json"
+        ]
+        
+        AF.request(BookRepository.bookBasePath, headers: headers)
+            .validate()
+            .responseDecodable(of: [BookShowcaseItem].self) { response in
+                switch response.result {
+                case .success:
+                    guard let bookShowcase = response.value else { return }
+                    completionHandler(bookShowcase)
+                
+                case let .failure(error):
+                    print(error)
+                }
+            }
+    }
+}
