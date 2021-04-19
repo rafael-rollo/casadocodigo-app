56472a7528d35d2e31bf86fe3e1fbc9948ad8592
Rafael Rollo <rafaelrollo92@gmail.com>
2021-04-19 13:07:27 -0300

    Added basic listing for authors

--- casadocodigo/Application/AuthorsScreen/AuthorsViewController.swift
+++ casadocodigo/Application/AuthorsScreen/AuthorsViewController.swift
@@ -7,12 +7,44 @@
 
 import UIKit
 
-class AuthorsViewController: UIViewController {
+class AuthorsViewController: UIViewController, UITableViewDataSource {
+    
+    // MARK: Attributes
+    
+    var authors: [Author] = []
+    
+    // MARK: IBOutlets
+    
+    @IBOutlet weak var authorsTableView: UITableView!
+    
+    // MARK: View lifecycle methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.authorsTableView.dataSource = self
         
         StatusBarBackground(target: self.view).set(color: NavigationBar.COLOR)
+        
+        AuthorRepository().allAuthors { authors in
+            self.authors = authors
+            self.authorsTableView.reloadData()
+        } failureHandler: {
+            Alert.show(title: "Ops", message: "Could not possible to load our authors", in: self)
+        }
     }
-
+    
+    // MARK: UITVDataSource impl
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.authors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let author = self.authors[indexPath.row]
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = author.firstName
+        
+        return cell
+    }
 }
