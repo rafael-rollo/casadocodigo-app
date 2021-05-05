d5669386c1dd0612f47c88cda09475268524e08d
Rafael Rollo <rafaelrollo92@gmail.com>
2021-05-05 14:23:14 -0300

    Book's complete data is presented on book details scene

--- casadocodigo/Application/BookDetailsScene/BookDetailsViewController.swift
+++ casadocodigo/Application/BookDetailsScene/BookDetailsViewController.swift
@@ -18,14 +18,59 @@ class BookDetailsViewController: UIViewController {
     @IBOutlet weak var subtitleLabel: UILabel!
     @IBOutlet weak var coverImageView: UIImageView!
     
+    @IBOutlet weak var ebookPrice: UILabel!
+    @IBOutlet weak var hardcoverPrice: UILabel!
+    @IBOutlet weak var comboPrice: UILabel!
+    
+    @IBOutlet weak var contentSectionTitle: SectionTitle!
+    @IBOutlet weak var contentTextView: UITextView!
+    
+    @IBOutlet weak var authorSectionTitle: SectionTitle!
+    @IBOutlet weak var authorImageView: UIImageView!
+    @IBOutlet weak var authorNameLabel: UILabel!
+    
+    @IBOutlet weak var productInfoSectionTitle: SectionTitle!
+    @IBOutlet weak var numberOfPagesLabel: UILabel!
+    @IBOutlet weak var ISBNLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         StatusBarBackground(target: self.view).set(color: NavigationBar.COLOR)
         navigationBar.configure(navigationController, current: self)
         
-        guard let selectedBook = selectedBook else { return }
-        titleLabel.text = selectedBook.title
-        coverImageView.af.setImage(withURL: selectedBook.coverImagePath)
+        if let selectedBook = selectedBook {
+            buildScreenUsing(selectedBook)
+        }
+    }
+    
+    private func buildScreenUsing(_ book: BookResponse) {
+        titleLabel.text = book.title
+        subtitleLabel.text = book.subtitle
+        coverImageView.af.setImage(withURL: book.coverImagePath)
+        
+        book.prices.forEach { price in
+            switch price.bookType {
+            case .ebook:
+                ebookPrice.text = price.value.currencyValue()
+            
+            case .hardCover:
+                hardcoverPrice.text = price.value.currencyValue()
+                
+            case .combo:
+                comboPrice.text = price.value.currencyValue()
+            }
+        }
+        
+        contentSectionTitle.label.text = "Conteúdo"
+        contentTextView.text = book.description
+        
+        authorSectionTitle.label.text = "Autor"
+        authorImageView.af.setImage(withURL: book.author.profilePicturePath)
+        authorNameLabel.text = book.author.fullName
+        
+        productInfoSectionTitle.label.text = "Dados do Produto"
+        numberOfPagesLabel.text = String(describing: book.numberOfPages)
+        ISBNLabel.text = book.ISBN
     }
 }
