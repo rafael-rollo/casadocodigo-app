b330509740b783d82b5bac0691fe31e1ffd88bad
Rafael Rollo <rafaelrollo92@gmail.com>
2021-05-05 17:15:11 -0300

    Added form section title

--- casadocodigo/Application/BookFormScene/BookFormViewController.swift
+++ casadocodigo/Application/BookFormScene/BookFormViewController.swift
@@ -9,13 +9,22 @@ import UIKit
 
 class BookFormViewController: UIViewController {
 
+    // MARK: IBOutlets
+    
     @IBOutlet weak var navigationBar: NavigationBar!
+    @IBOutlet weak var sectionTitle: SectionTitle!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         StatusBarBackground(target: self.view).set(color: NavigationBar.COLOR)
         navigationBar.configure(navigationController, current: self)
+        
+        buildUp()
+    }
+    
+    private func buildUp() {
+        sectionTitle.label.text = "Novo Livro"
     }
 
 }
