2720272465656fe65ccca590374ede9ecd16e184
Rafael Rollo <rafaelrollo92@gmail.com>
2021-05-04 23:08:41 -0300

    Implemented base layout

--- casadocodigo/Application/BookDetailsScene/BookDetailsViewController.swift
+++ casadocodigo/Application/BookDetailsScene/BookDetailsViewController.swift
@@ -6,13 +6,17 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class BookDetailsViewController: UIViewController {
     
     var selectedBook: BookShowcaseItem?
     
     @IBOutlet weak var navigationBar: NavigationBar!
+    
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var coverImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +24,8 @@ class BookDetailsViewController: UIViewController {
         StatusBarBackground(target: self.view).set(color: NavigationBar.COLOR)
         navigationBar.configure(navigationController, current: self)
         
-        if let book = selectedBook {
-            titleLabel.text = book.title
-        }
+        guard let selectedBook = selectedBook else { return }
+        titleLabel.text = selectedBook.title
+        coverImageView.af.setImage(withURL: selectedBook.coverImagePath)
     }
 }
