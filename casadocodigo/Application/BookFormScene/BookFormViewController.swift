99bb9670a0b233964ff19cefa29706559b4b70f8
Rafael Rollo <rafaelrollo92@gmail.com>
2021-05-05 21:46:53 -0300

    Authors picker view fulfilled by the authors list

--- casadocodigo/Application/BookFormScene/BookFormViewController.swift
+++ casadocodigo/Application/BookFormScene/BookFormViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 import AlamofireImage
 
 class BookFormViewController: UIViewController {
+    
+    // MARK: Attributes
+    
+    var authors: [AuthorResponse] = []
+    var authorRepository: AuthorRepository
 
     // MARK: IBOutlets
     
@@ -20,19 +25,40 @@ class BookFormViewController: UIViewController {
     
     @IBOutlet weak var descriptionTextView: UITextView!
     
+    @IBOutlet weak var authorTextField: UITextField!
+    var authorPickerView = UIPickerView()
+    
+    // MARK: Initializers
+    
+    init(authorRepository: AuthorRepository = AuthorRepository(), nibName: String? = nil, bundle: Bundle? = nil) {
+        self.authorRepository = authorRepository
+        super.init(nibName: nibName, bundle: bundle)
+    }
+    
+    required init?(coder: NSCoder) {
+        self.authorRepository = AuthorRepository()
+        super.init(coder: coder)
+    }
+    
     // MARK: View lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        authorPickerView.dataSource = self
+        authorPickerView.delegate = self
+        
         StatusBarBackground(target: self.view).set(color: NavigationBar.COLOR)
         navigationBar.configure(navigationController, current: self)
         
+        loadAuthors()
         buildUp()
     }
     
     private func adjustLayout() {
         descriptionTextView.configureBorders()
+        
+        authorTextField.inputView = authorPickerView
     }
     
     private func buildUp() {
@@ -50,5 +76,47 @@ class BookFormViewController: UIViewController {
         guard let coverUri = URL(string: coverPathAsString) else { return }
         
         coverImageView.af.setImage(withURL: coverUri)
+    }
+    
+    
+    
+    // MARK: View methods
+    
+    func updateAuthors(with authors: [AuthorResponse]) {
+        self.authors = authors
+        
+        guard let firstAuthor = authors.first else { return }
+        
+        authorPickerView.selectRow(0, inComponent: 0, animated: false)
+        authorTextField.text = firstAuthor.fullName
+    }
+    
+    func loadAuthors() {
+        authorRepository.allAuthors { [weak self] loadedAuthors in
+            guard let self = self else { return }
+            self.updateAuthors(with: loadedAuthors)
+            
+        } failureHandler: {
+            Alert.show(title: "Ops", message: "Could not possible to load our authors", in: self)
+        }
+    }
+}
+
+extension BookFormViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1 // single picker component
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return authors.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return authors[row].fullName
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        authorTextField.text = authors[row].fullName
+        authorTextField.resignFirstResponder()
     }
 }
