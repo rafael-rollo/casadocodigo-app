23756c33ea41081739efc289fed19102999c35a0
Rafael Rollo <rafaelrollo92@gmail.com>
2021-07-13 16:04:53 -0300

    Refactored the CustomNavigationController's view code design

--- casadocodigo/Application/CustomNavigationController.swift
+++ casadocodigo/Application/CustomNavigationController.swift
@@ -9,23 +9,54 @@ import UIKit
 
 class CustomNavigationController: UINavigationController {
     
-    var titleView: UIView!
+    private lazy var titleImageView: UIImageView = {
+        let image = UIImage(named: "text-logo")
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private lazy var titleView: UIView = {
+        let view = UIView(frame: CGRect.zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(titleImageView)
+        return view
+    }()
     
     override var preferredStatusBarStyle : UIStatusBarStyle {
         return .lightContent
     }
+    
+    override func loadView() {
+        super.loadView()
+        setup()
+    }
+}
 
-    override init(nibName: String?, bundle: Bundle?) {
-        super.init(nibName: nibName, bundle: bundle)
-        setupNavigation()
+extension CustomNavigationController: ViewCode {
+    func addViews() {
+        view.addSubview(titleView)
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setupNavigation()
+    func addConstraints() {
+        NSLayoutConstraint.activate([
+            titleView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            titleView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleView.heightAnchor.constraint(equalToConstant: navigationBar.frame.size.height),
+            titleView.widthAnchor.constraint(greaterThanOrEqualTo: view.widthAnchor,
+                                             multiplier: 0.70)
+        ])
+
+        NSLayoutConstraint.activate([
+            titleImageView.heightAnchor.constraint(equalTo: titleView.heightAnchor, multiplier: 0.8),
+            titleImageView.widthAnchor.constraint(equalTo: titleView.heightAnchor),
+            titleImageView.centerXAnchor.constraint(equalTo: titleView.centerXAnchor),
+            titleImageView.centerYAnchor.constraint(equalTo: titleView.centerYAnchor)
+        ])
     }
     
-    fileprivate func setupNavigation() {
+    func addTheme() {
         let appearance = UINavigationBarAppearance()
         appearance.backgroundColor = UIColor.orangeColor
         appearance.shadowColor = .none
@@ -41,31 +72,6 @@ class CustomNavigationController: UINavigationController {
                                       NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13.0)]
         
         UIBarButtonItem.appearance().tintColor = UIColor.white
-        
-        titleView = UIView(frame: CGRect.zero)
-        titleView.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(titleView)
-        NSLayoutConstraint.activate([
-            titleView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            titleView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            titleView.heightAnchor.constraint(equalToConstant: navigationBar.frame.size.height),
-            titleView.widthAnchor.constraint(greaterThanOrEqualTo: view.widthAnchor,
-                                             multiplier: 0.70)
-        ])
-        
-        let title = UIImage(named: "text-logo")
-        let titleImageView = UIImageView(image: title)
-        titleImageView.contentMode = .scaleAspectFill
-        titleImageView.translatesAutoresizingMaskIntoConstraints = false
-
-        titleView.addSubview(titleImageView)
-        NSLayoutConstraint.activate([
-            titleImageView.heightAnchor.constraint(equalTo: titleView.heightAnchor, multiplier: 0.8),
-            titleImageView.widthAnchor.constraint(equalTo: titleView.heightAnchor),
-            titleImageView.centerXAnchor.constraint(equalTo: titleView.centerXAnchor),
-            titleImageView.centerYAnchor.constraint(equalTo: titleView.centerYAnchor)
-        ])
     }
 }
 
@@ -73,9 +79,13 @@ extension UIViewController {
     func setupNavigationBar(itemsOnTheLeft: [NavigationBarItem] = [],
                             itemsOnTheRight: [NavigationBarItem] = []) {
         let parentOrSelf = parent is UITabBarController ? parent : self
-        parentOrSelf?.navigationItem.setLeftBarButtonItems(itemsOnTheLeft.map { $0.toUIBarButtonItem() },
-                                                           animated: true)
-        parentOrSelf?.navigationItem.setRightBarButtonItems(itemsOnTheRight.map { $0.toUIBarButtonItem() },
-                                                            animated: true)
+        
+        parentOrSelf?.navigationItem.setLeftBarButtonItems(
+            itemsOnTheLeft.map { $0.toUIBarButtonItem() }, animated: true
+        )
+        
+        parentOrSelf?.navigationItem.setRightBarButtonItems(
+            itemsOnTheRight.map { $0.toUIBarButtonItem() }, animated: true
+        )
     }
 }
